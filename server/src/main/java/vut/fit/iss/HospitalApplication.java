07cea57d107d3277a512cf322819310e43d8af28
Dissupos <galepwnz@gmail.com>
2016-12-04 12:18:34 +0100

    options

--- server/src/main/java/vut/fit/iss/HospitalApplication.java
+++ server/src/main/java/vut/fit/iss/HospitalApplication.java
@@ -28,38 +28,6 @@ public class HospitalApplication {
         SpringApplication.run(HospitalApplication.class, args);
     }
 
-//    @Bean
-//    FilterRegistrationBean corsFilter(
-//            @Value("${tagit.origin:http://localhost:9000}") String origin) {
-//        return new FilterRegistrationBean(new Filter() {
-//            public void doFilter(ServletRequest req, ServletResponse res,
-//                                 FilterChain chain) throws IOException, ServletException {
-//                HttpServletRequest request = (HttpServletRequest) req;
-//                HttpServletResponse response = (HttpServletResponse) res;
-//                String method = request.getMethod();
-//                response.setHeader("Access-Control-Allow-Origin", origin);
-//                response.setHeader("Access-Control-Allow-Methods",
-//                        "POST,GET,PUT,OPTIONS,DELETE");
-//                response.setHeader("Access-Control-Max-Age", Long.toString(60 * 60));
-//                response.setHeader("Access-Control-Allow-Credentials", "true");
-//                response.setHeader(
-//                        "Access-Control-Allow-Headers",
-//                        "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
-//                if ("OPTIONS".equals(method)) {
-//                    response.setStatus(HttpStatus.OK.value());
-//                } else {
-//                    chain.doFilter(req, res);
-//                }
-//            }
-//
-//            public void init(FilterConfig filterConfig) {
-//            }
-//
-//            public void destroy() {
-//            }
-//        });
-//    }
-
     @Bean
     CommandLineRunner init(final UserService userService, final AccountService accountService, final DepartmentService departmentService) {
 
--- server/src/main/java/vut/fit/iss/controller/ServiceResource.java
+++ server/src/main/java/vut/fit/iss/controller/ServiceResource.java
@@ -2,8 +2,11 @@ package vut.fit.iss.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import vut.fit.iss.service.user.account.AccountService;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class ServiceResource {
@@ -18,5 +21,13 @@ public class ServiceResource {
     @RequestMapping
     public String index() {
         return "Home Page";
+    }
+
+    @RequestMapping(value = "/api/**", method = RequestMethod.OPTIONS)
+    public void corsHeaders(HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with");
+        response.addHeader("Access-Control-Max-Age", "3600");
     }
 }
