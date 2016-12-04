bc5146c3326e146d244f7dc25a3dd6b0bd92c0b2
Dissupos <galepwnz@gmail.com>
2016-12-04 12:11:04 +0100

    options

--- server/src/main/java/vut/fit/iss/HospitalApplication.java
+++ server/src/main/java/vut/fit/iss/HospitalApplication.java
@@ -1,13 +1,10 @@
 package vut.fit.iss;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import vut.fit.iss.domain.other.Department;
 import vut.fit.iss.domain.user.Patient;
 import vut.fit.iss.domain.user.PatientStatus;
@@ -20,10 +17,6 @@ import vut.fit.iss.service.other.DepartmentService;
 import vut.fit.iss.service.user.UserService;
 import vut.fit.iss.service.user.account.AccountService;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Date;
 
 @Configuration
@@ -35,37 +28,37 @@ public class HospitalApplication {
         SpringApplication.run(HospitalApplication.class, args);
     }
 
-    @Bean
-    FilterRegistrationBean corsFilter(
-            @Value("${tagit.origin:http://localhost:9000}") String origin) {
-        return new FilterRegistrationBean(new Filter() {
-            public void doFilter(ServletRequest req, ServletResponse res,
-                                 FilterChain chain) throws IOException, ServletException {
-                HttpServletRequest request = (HttpServletRequest) req;
-                HttpServletResponse response = (HttpServletResponse) res;
-                String method = request.getMethod();
-                response.setHeader("Access-Control-Allow-Origin", origin);
-                response.setHeader("Access-Control-Allow-Methods",
-                        "POST,GET,PUT,OPTIONS,DELETE");
-                response.setHeader("Access-Control-Max-Age", Long.toString(60 * 60));
-                response.setHeader("Access-Control-Allow-Credentials", "true");
-                response.setHeader(
-                        "Access-Control-Allow-Headers",
-                        "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
-                if ("OPTIONS".equals(method)) {
-                    response.setStatus(HttpStatus.OK.value());
-                } else {
-                    chain.doFilter(req, res);
-                }
-            }
-
-            public void init(FilterConfig filterConfig) {
-            }
-
-            public void destroy() {
-            }
-        });
-    }
+//    @Bean
+//    FilterRegistrationBean corsFilter(
+//            @Value("${tagit.origin:http://localhost:9000}") String origin) {
+//        return new FilterRegistrationBean(new Filter() {
+//            public void doFilter(ServletRequest req, ServletResponse res,
+//                                 FilterChain chain) throws IOException, ServletException {
+//                HttpServletRequest request = (HttpServletRequest) req;
+//                HttpServletResponse response = (HttpServletResponse) res;
+//                String method = request.getMethod();
+//                response.setHeader("Access-Control-Allow-Origin", origin);
+//                response.setHeader("Access-Control-Allow-Methods",
+//                        "POST,GET,PUT,OPTIONS,DELETE");
+//                response.setHeader("Access-Control-Max-Age", Long.toString(60 * 60));
+//                response.setHeader("Access-Control-Allow-Credentials", "true");
+//                response.setHeader(
+//                        "Access-Control-Allow-Headers",
+//                        "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
+//                if ("OPTIONS".equals(method)) {
+//                    response.setStatus(HttpStatus.OK.value());
+//                } else {
+//                    chain.doFilter(req, res);
+//                }
+//            }
+//
+//            public void init(FilterConfig filterConfig) {
+//            }
+//
+//            public void destroy() {
+//            }
+//        });
+//    }
 
     @Bean
     CommandLineRunner init(final UserService userService, final AccountService accountService, final DepartmentService departmentService) {
