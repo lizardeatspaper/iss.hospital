00af67217d2f944285e2cc2e4c45d8d81d2f5412
Dissupos <galepwnz@gmail.com>
2016-12-04 13:19:22 +0100

    test

---
+++ server/src/main/java/vut/fit/iss/config/StaticResourceConfiguration.java
@@ -0,0 +1,14 @@
+package vut.fit.iss.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
--- server/src/main/java/vut/fit/iss/controller/ServiceResource.java
+++ server/src/main/java/vut/fit/iss/controller/ServiceResource.java
@@ -2,7 +2,6 @@ package vut.fit.iss.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import vut.fit.iss.service.user.account.AccountService;
 
 @Controller
@@ -15,10 +14,10 @@ public class ServiceResource {
         this.service = service;
     }
 
-    @RequestMapping
-    public String index() {
-        return "index";
-    }
+//    @RequestMapping
+//    public String index() {
+//        return "index";
+//    }
 
 //    @RequestMapping(
 //            value = "/**",
