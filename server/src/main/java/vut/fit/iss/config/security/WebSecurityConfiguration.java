1beb159359b427f03ecb96bc77c100210b4cf4b5
Dissupos <galepwnz@gmail.com>
2016-12-04 12:26:18 +0100

    change corsOptions

--- server/src/main/java/vut/fit/iss/config/security/WebSecurityConfiguration.java
+++ server/src/main/java/vut/fit/iss/config/security/WebSecurityConfiguration.java
@@ -73,7 +73,7 @@ public class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdap
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**");
+                registry.addMapping("/**");
             }
         };
     }
--- server/src/main/java/vut/fit/iss/controller/ServiceResource.java
+++ server/src/main/java/vut/fit/iss/controller/ServiceResource.java
@@ -2,11 +2,8 @@ package vut.fit.iss.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import vut.fit.iss.service.user.account.AccountService;
-
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class ServiceResource {
@@ -23,11 +20,4 @@ public class ServiceResource {
         return "Home Page";
     }
 
-    @RequestMapping(value = "/api/**", method = RequestMethod.OPTIONS)
-    public void corsHeaders(HttpServletResponse response) {
-        response.addHeader("Access-Control-Allow-Origin", "*");
-        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with");
-        response.addHeader("Access-Control-Max-Age", "3600");
-    }
 }
