44eb0e8875967a25f7a718523a887578d974730f
vladbulyukhin <vlad.bulyukhin@yahoo.com>
2016-12-04 14:13:06 +0100

    corrected gitignore and api service

--- client/src/js/services/api-service.js
+++ client/src/js/services/api-service.js
@@ -3,6 +3,8 @@ angular.module('iss.hospital').factory('apiService', [
 	function apiServiceFactory($http) {
 		'use strict';
 
+		var serverUrl = window.location.protocol + '//' + window.location.host;
+
 		var apiService = {
 			login: login,
 			getLoggedUser: getLoggedUser
@@ -33,13 +35,15 @@ angular.module('iss.hospital').factory('apiService', [
 				config.headers['Content-Type'] = 'application/json';
 			}
 
+			config.url = serverUrl + config.url;
+
 			return $http(config);
 		}
 
 		function login(data) {
 			return request({
 				method: 'POST',
-				url: 'http://localhost:8080/api/login',
+				url: '/login',
 				data: data
 			});
 		}
@@ -47,7 +51,7 @@ angular.module('iss.hospital').factory('apiService', [
 		function getLoggedUser() {
 			return request({
 				method: 'GET',
-				url: 'http://localhost:8080/api/user'
+				url: '/api/user'
 			});
 		}
 	}
