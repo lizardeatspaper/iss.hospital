aef6ba66985e8f26447a96e46635fd8fbcc073ac
Dissupos <galepwnz@gmail.com>
2016-12-04 12:39:49 +0100

    cors

--- server/src/main/java/vut/fit/iss/controller/ServiceResource.java
+++ server/src/main/java/vut/fit/iss/controller/ServiceResource.java
@@ -1,12 +1,12 @@
 package vut.fit.iss.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import vut.fit.iss.service.user.account.AccountService;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class ServiceResource {
@@ -27,8 +27,11 @@ public class ServiceResource {
             value = "/**",
             method = RequestMethod.OPTIONS
     )
-    public ResponseEntity handle() {
-        return new ResponseEntity(HttpStatus.OK);
+    public void corsHeaders(HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with");
+        response.addHeader("Access-Control-Max-Age", "3600");
     }
 
 }
