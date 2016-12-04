070dcef95373183b1e88454ff26bec3e4d80b008
Dissupos <galepwnz@gmail.com>
2016-12-04 13:31:03 +0100

    config

--- server/src/main/java/vut/fit/iss/config/StaticResourceConfiguration.java
+++ server/src/main/java/vut/fit/iss/config/StaticResourceConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/**").addResourceLocations("/");
     }
 }
