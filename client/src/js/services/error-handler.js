e1f2775da12e88a6d41d03c153389645878ab2ed
vladbulyukhin <vlad.bulyukhin@yahoo.com>
2016-12-05 23:18:31 +0100

    fix error handling

--- client/src/js/services/error-handler.js
+++ client/src/js/services/error-handler.js
@@ -9,17 +9,19 @@ angular.module('iss.hospital').factory('errorHandler', [
 				$state.go('hospital.authorization.login');
 			}
 
-			var notification;
-			if (angular.isObject(error)) {
-				notification = {
-					message: error.data.message,
-					title: error.data.error
-				};
+			if (angular.isObject(error.data)) {
+				if (angular.isArray(error.data.errors)) {
+					angular.forEach(error.data.errors, function(e) {
+						Notification.error({title: error.data.error, message: e.field + ': ' + e.defaultMessage})
+					});
+				} else {
+                    Notification.error({title: error.data.error, message: error.data.message})
+                }
+			} else if (error.status = 500) {
+				Notification.error({title: 'Internal Server Error', message: 'Please try later.'});
 			} else {
-				notification = error;
-			}
-
-			Notification.error(notification);
+                Notification.error({title: 'Error', message: 'The request has ended with an error.'});
+            }
 		};
 	}
 ]);
